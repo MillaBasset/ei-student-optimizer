20fcb4a4d23f95084b5b207d96c40c311da8a08f
Avifir <Niedzielan@users.noreply.github.com>
2021-03-23 01:31:41 +0000

    New optimiser
    Should give getter results for edge cases

--- script.js
+++ script.js
@@ -24,12 +24,26 @@ function optimize() {
     if (adBonus) { dt *= 1.5 }
     if (acceleration) { dt *= accelerationBonus }
 
+
     function getCost(num) {
         if (num % 2 === 1) {
             return (Math.pow(num, 2) - 1) / 4 + (num + 1) / 2
         } else {
             return (Math.pow(num, 2) + 2 * num) / 4
         }
+    }
+
+    function getNextCost(num) {
+        return getCost(num+1) - getCost(num);
+    }
+
+    function getCostOrder(order) {
+        cost = 0;
+        for (var i = 0; i < order.length-1; i++) {
+            cost += getCost(order[i]);
+        }
+        cost += 2*order[6]
+        return cost
     }
 
     var boosts = [
@@ -42,8 +56,15 @@ function optimize() {
         0.1
     ]
 
+    function getTotalBoost(order) {
+        res = 0
+        for (var i = 0; i < order.length-1; i++) {
+            res += order[i]*boosts[i]
+        }
+        return (1+order[6]*0.1)*res
+    }
+
     var order = [0, 0, 0, 0, 0, 0, 0]
-    var cost = 0
 
     if (!ignoreTheories) {
         if (students >= 60) {
@@ -57,33 +78,32 @@ function optimize() {
         }
     }
 
-    function getBest(order, remainingStudents) {
+    function getBestTotal(order, totalStudents, highest = [0,0,0,0,0,0,0]) {
+        var remainingStudents = totalStudents - getCostOrder(order)
         var bestBoostPerCost = 0
         var bestI = -1
-        var bestCost = 0
         for (var i = 0; i < order.length; i++) {
             if ((i >= 3 && i < 6 && order[i] === 8) || (i === 6 && order[i] === 6)) {
                 continue
             }
-            if (i === 6 && remainingStudents >= 2) {
+            var boostPerCost = 0;
+            var cost = 0;
+            if (i === 6) {
                 bestPerCost = 0.05 * (
                     order[0] * boosts[0] + order[1] * boosts[1] + order[2] * boosts[2] +
                     order[3] * boosts[3] + order[4] * boosts[4] + order[5] * boosts[5]
                 )
                 cost = 2
             }
-            else if (i == 6) {
-                continue
-            }
             else {
-                cost = (getCost(order[i] + 1) - getCost(order[i]))
-                if (cost > remainingStudents) {
-                    continue
-                }
+                cost = getNextCost(order[i])
                 extraBoost = (1 + order[6] * boosts[6]) * boosts[i]
                 bestPerCost = extraBoost / cost
             }
             if (bestPerCost > bestBoostPerCost) {
+                if (cost > remainingStudents) {
+                    continue
+                }
                 bestBoostPerCost = bestPerCost
                 bestI = i
                 bestCost = cost
@@ -91,17 +111,91 @@ function optimize() {
         }
         if (bestI !== -1) {
             order[bestI] += 1
-            return order, remainingStudents - bestCost
+            if (getTotalBoost(order) > getTotalBoost(highest)) {
+                highest = order.slice()
+            }
+            return [order, totalStudents, highest]
         }
         else {
-            return order, 0
+            return [order, 0, highest]
         }
     }
 
-    var remainingStudents = students
-    while (remainingStudents > 0) {
-        order, remainingStudents = getBest(order, remainingStudents)
+    var tempStudents = students
+    var highest = order.slice()
+    while (tempStudents > 0) {
+        var res = getBestTotal(order, tempStudents, highest)
+        order = res[0]
+        tempStudents = res[1]
+        highest = res[2]
     }
+
+    function contains(arrayOfArrays, searchArray) {
+        var res = false
+        for (var i = 0; i < arrayOfArrays.length; i++) {
+            var array1 = arrayOfArrays[i]
+            res = array1.length === searchArray.length && array1.every(function(value, index) { return value === searchArray[index]})
+            if (res) {
+                return true
+            }
+        }
+        return false
+
+    }
+
+    function getAllForks(order, students, highest, required = [0,0,0,0,0,0,0]){
+        var maxBuy = [-1, -1, -1, 8, 8, 8, 6]
+        var tmp = []
+        for (var i = 0; i < order.length; i++) {
+            tmp1 = order.slice()
+            tmp1[i] = order[i] +1
+            if (maxBuy[i] > 0) {
+                tmp1[i] = Math.min(maxBuy[i], tmp1[i])
+            }
+            if (getTotalBoost(tmp1) < getTotalBoost(highest)) {
+                continue
+            }
+            tmp2 = required.slice()
+            tmp2[i] = order[i]+1
+            if (maxBuy[i] > 0) {
+                tmp2[i] = Math.min(maxBuy[i], tmp2[i] )
+            }
+            if (getCostOrder(tmp2) > students) {
+                continue
+            }
+            if (!contains(tmp,tmp2)) {
+                tmp.push(tmp2)
+            }
+        }
+        return tmp
+    }
+
+    var possibles = getAllForks(highest, students, highest)
+    var done_possibles = []
+
+    while (possibles.length > 0) {
+        var possible = possibles.shift()
+        done_possibles.push(possible.slice())
+        order = possible.slice()
+        total_students = students
+        while (total_students > 0) {
+            var res = getBestTotal(order, total_students, highest)
+            order = res[0]
+            total_students = res[1]
+            highest = res[2]
+        }
+        tmp_possibles = getAllForks(order, students, highest, possible)
+        for (var tmp_possible_i = 0; tmp_possible_i < tmp_possibles.length; tmp_possible_i++) {
+            var tmp_possible = tmp_possibles[tmp_possible_i]
+            if (contains(possibles, tmp_possible) || contains(done_possibles,tmp_possible)) {
+                continue
+            }
+            else {
+                possibles.push(tmp_possible)
+            }
+        }
+    }
+    order = highest
 
     var phi = (
         (1 + order[6] * boosts[6]) *
